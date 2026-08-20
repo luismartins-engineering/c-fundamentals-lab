69cd7d02edd43eb8d62d9a80503728268f222259
Luís Martins <luismmartins304@gmail.com>
2026-08-20 20:30:40 +0100

    feat: complete basic operations calculator

--- 01_consolidation/001_basic_operations/src/main.c
+++ 01_consolidation/001_basic_operations/src/main.c
@@ -1,42 +1,95 @@
-#include "stdio.h"
+#include <stdio.h>
 
 int main(){
 
-    printf("Hello world!\n\n");
+    printf("Calculator\n\n");
 
-    float op1, op2, result;
+    int op1, op2, result;
     int opr;
+    int valid = 0;
+    int result_valid = 0;
 
-    printf("First Operand: ");
-    scanf("%f", &op1);
-    printf("Second Operand: ");
-    scanf("%f", &op2);
+    while (!valid)
+    {
+        valid = 1;
 
-    printf("\n 1 - Sum\n 2 - Subtraction\n 3 - multiplication\n 4 - Division\nChoose the operation: ");
+        printf("First Operand: ");
+        if (scanf("%d", &op1) != 1)
+        {
+            printf("OP1 is not a valid integer.\n");
+            valid = 0;
+
+            while (getchar() != '\n')
+            {
+            }
+        }
+
+        if (valid)
+        {
+            printf("Second Operand: ");
+            if (scanf("%d", &op2) != 1)
+            {
+                printf("OP2 is not a valid integer.\n");
+                valid = 0;
+
+                while (getchar() != '\n')
+                {
+                }
+            }
+        }
+    }
+
+   
+    printf("\n 1 - Sum\n 2 - Subtraction\n 3 - multiplication\n 4 - Division\n 5 - Remainder\nChoose the operation: ");
     scanf("%d", &opr);
 
-    if (opr == 1){
+    switch (opr)
+    {
+    case 1:
         result = op1 + op2;
-    }
-    if (opr == 2){
+        result_valid = 1;
+        break;
+    case 2:
         result = op1 - op2;
-    }
-    if (opr == 3){
+        result_valid = 1;
+        break;
+    case 3:
         result = op1 * op2;
-    }
-    if (opr == 4){
+        result_valid = 1;
+        break;
+    case 4:
         if(op2 == 0)
         {
             printf("Impossible divided by 0\n");
-            return 0;
         }
         else
         {
             result = op1 / op2;
+            result_valid = 1;
         }
+        break;
+    case 5: 
+        if(op2 == 0)
+        {
+            printf("Impossible divided by 0\n");
+        }
+        else
+        {
+            result = op1 % op2;
+            result_valid = 1;
+        }
+        break;
+    default:
+        printf(" Invalid Choice \n");
+        break;
     }
 
-    printf("\nThe result of the operation is %.2f\n", result );
+    if (result_valid == 1)
+    {
+        printf("\nThe result of the operation is %d\n", result );
+    }
+
+    
     
 
     return 0; 
