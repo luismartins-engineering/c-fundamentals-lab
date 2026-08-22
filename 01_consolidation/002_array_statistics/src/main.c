6c3184eb6b4efd1b61d931991b1cfeb89b5b5641
Luís Martins <luismmartins304@gmail.com>
2026-08-22 16:39:49 +0100

    complete array statistics exercise

--- 01_consolidation/002_array_statistics/src/main.c
+++ 01_consolidation/002_array_statistics/src/main.c
@@ -2,16 +2,77 @@
 
 int main (){
 
-    printf("Hello world");
-    int n[9] = {};
-    int c = 0;
+    int array[10] = {0};
+    int lenght = 10;
+    float sum = 0;
+    float avg = 0;
+    int pos = 0, neg = 0, zeros = 0;
+    int valid = 0;
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < lenght; i++)
     {
-        printf("\nPut a number: ");
-        scanf("%d", &n[c]);
-        c++;
+        valid = 0;
+
+        while (!valid)
+        {
+            printf("Put a number: ");
+
+            if (scanf("%d", &array[i]) != 1)
+            {
+                printf("is not a valid integer.\n");
+
+                while (getchar() != '\n')
+                {
+                }
+            }
+            else
+            {
+                valid = 1;
+            }
+        }
     }
-    
-    printf("%d",n[3]);
+
+    int max = array[0];
+    int min = array[0];
+
+    for(int i = 0; i < lenght; i++)
+    {
+       if(max < array[i])
+       {
+            max = array[i];
+       }
+
+       if(min > array[i])
+       {
+            min = array[i];
+       }
+
+       if(array[i] > 0)
+       {
+        pos++;
+       }
+       
+       if(array[i] < 0)
+       {
+        neg++;
+       }
+
+       if(array[i] == 0)
+       {
+        zeros++;
+       }
+
+       sum += array[i];
+       
+    }
+
+    avg = sum / lenght;
+
+    printf("Maximum: %d\n",max);
+    printf("Minimum: %d\n",min);
+    printf("Sum: %.1f\n",sum );
+    printf("Average: %.2f\n",avg );
+    printf("Positive values: %d\n",pos);
+    printf("Negative values: %d\n",neg);
+    printf("Zeros:  %d\n",zeros);
 }
